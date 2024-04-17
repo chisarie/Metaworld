8689de57e5a6aff582cdfd91e7289f37bbcbed4a
Eugenio <chisari@cs.uni-freiburg.de>
2024-04-17 10:13:56 +0200

    fixed policy lever-pull-v2

--- metaworld/policies/sawyer_lever_pull_v2_policy.py
+++ metaworld/policies/sawyer_lever_pull_v2_policy.py
@@ -12,7 +12,8 @@ class SawyerLeverPullV2Policy(Policy):
             "hand_pos": obs[:3],
             "gripper": obs[3],
             "lever_pos": obs[4:7],
-            "unused_info": obs[7:],
+            "unused_info": obs[7:-3],
+            "goal_pos": obs[-3:],
         }
 
     def get_action(self, obs):
@@ -31,10 +32,13 @@ class SawyerLeverPullV2Policy(Policy):
     def _desired_pos(o_d):
         pos_curr = o_d["hand_pos"]
         pos_lever = o_d["lever_pos"] + np.array([0.0, -0.055, 0.0])
+        pos_goal = o_d["goal_pos"]
 
         if np.linalg.norm(pos_curr[:2] - pos_lever[:2]) > 0.02:
             return pos_lever + np.array([0.0, 0.0, -0.1])
         elif abs(pos_curr[2] - pos_lever[2]) > 0.02:
             return pos_lever
-        else:
+        elif pos_lever[0] - pos_goal[0] < -0.3:
             return pos_lever + np.array([0.0, 0.08, 0.02])
+        else:
+            return pos_goal + np.array([0.0, -0.055, 0.0])
